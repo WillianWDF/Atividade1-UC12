607bcec816659b95f962d27f28020015d4cf4235
WillianWDF <williandamazio794@outlook.com>
2022-10-02 22:07:50 -0300

    Correções dos erros propostos

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text, Button} from 'react-native';
+  import MyList from './src/components/MyList.js';
+
+
+export default class App extends Component
+{
+
+  render(){
+    return(
+        <MyList />   
+        
+    );
+  }
+}
+
+
